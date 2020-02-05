e93b5ae36182de9ac83e20e210825e56cbc7cd02
illuhich <49656277+illuhich@users.noreply.github.com>
2020-02-05 18:52:11 +0200

    create registration system

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+import django.contrib.auth.views as user
 from . import views
  
-app_name = 'account'
+# app_name = 'account'
 
 urlpatterns = [
-	path('login/', LoginView.as_view(), name="login"),
-	path('logout/', LogoutView.as_view(), name="logout"),
+	path('login/', user.LoginView.as_view(), name="login"),
+	path('logout/', user.LogoutView.as_view(), name="logout"),
 	path('dashboard/', views.dashboard, name='dashboard'),
+	path('password-change/', user.PasswordChangeView.as_view(), name="password_change"),
+	path('password-change/done/', user.PasswordChangeDoneView.as_view(), name="password_change_done"),
+	path('password-reset/', user.PasswordResetView.as_view(), name="password_reset"),
+	path('password-reset/done/', user.PasswordResetDoneView.as_view(), name="password_reset_done"),
+	path('reset/<uidb64>/<token>/', user.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+	path('reset/done/', user.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+	path('register/', views.register, name='register'),
 ]
