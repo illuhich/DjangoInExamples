3471d1866faf2b3f8817a6cdd665b7ce168036b8
illuhich <49656277+illuhich@users.noreply.github.com>
2020-04-09 12:18:50 +0300

    add coupon app

--- demo_shop/cart/cart.py
+++ demo_shop/cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -13,6 +14,8 @@ class Cart(object):
             # Сохраняю пустую корзину в сессии
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # Сохранить применяемы купон
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, update_quantity=False):
         """Добавить товар в корзину или обновить его количество"""
@@ -63,3 +66,18 @@ class Cart(object):
         """Удаление корзины из сессии"""
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
+    
--- demo_shop/orders/admin.py
+++ demo_shop/orders/admin.py
@@ -3,7 +3,16 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
+
+def order_detail(obj):
+	return mark_safe('<a href="{}">View</a>'.format(reverse('orders:admin_order_detail', args=[obj.id])))
+
+def order_pdf(obj):
+	return mark_safe('<a href="{}">PDF</a>'.format(reverse('orders:admin_order_pdf', args=[obj.id])))
+order_pdf.short_description = 'Invoice'
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -32,7 +41,7 @@ class OrderItemInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code',
-                    'city', 'paid', 'created', 'updated']
+                    'city', 'paid', 'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
